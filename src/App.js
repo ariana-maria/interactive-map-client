b880e66de014a3c6fdefac0460713df1f7f05acc
Ariana Brandao <61847161+ariana-maria@users.noreply.github.com>
2023-06-14 14:45:21 -0700

    added zip code API to App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  //states are important in all FE programming to know the state of the page.
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+
+      try {
+        const response = await fetch('http://ZiptasticAPI.com/73013');
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        setError(error);
+      }
+
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          This is Ariana's Interactive map client.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Ariana's Interactive Map Client</h1>
+     {data && (
+        <div>
+          <h2>Zip Code: {data.zip}</h2>
+          <p>City: {data.city}</p>
+          <p>State: {data.state}</p>
+        </div>
+      )}
     </div>
   );
 }
